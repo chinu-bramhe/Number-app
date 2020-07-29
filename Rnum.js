53fa135ea7dc9bf3bcaef548c8dec096da70f535
chinu-bramhe <68947565+chinu-bramhe@users.noreply.github.com>
2020-07-29 16:54:20 +0530

    Add files via upload

--- Rnum.js
+++ Rnum.js
@@ -2,10 +2,28 @@ import React from 'react'
 import Style from '../assets/Numui.css';
 function Rnum(props) {
     return (
+        
         <div className="grid">
-        <div className="cont" style={{background:props.colo}}>
+        {props.tag === "Cold" && (
+            <div className="cont" style={{background:props.colo}}>
             <h1>{props.tag}</h1>
         </div>
+          )}
+          {props.tag === "Correct" && (
+            <div className="cont" style={{background:props.colo}}>
+            <h1>{props.tag}</h1>
+        </div>
+          )}
+          {props.tag === "Warm" && (
+            <div className="cont" style={{background:props.colo}}>
+            <h1>{props.tag}</h1>
+        </div>
+          )}{props.tag === "Hot" && (
+            <div className="cont" style={{background:props.colo}}>
+            <h1>{props.tag}</h1>
+        </div>
+          )}
+        
         </div>
     )
 }
