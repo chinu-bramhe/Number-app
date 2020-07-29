b2b3659a030639a32348ffa1c28ecde2feab159a
chinu-bramhe <68947565+chinu-bramhe@users.noreply.github.com>
2020-07-29 15:11:06 +0530

    Add files via upload
    Check the number severity correct ,warm ,cold ,hot.

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import RandomNum from './RandomNumApp/RandomNum.js'
+import Rnum from './RandomNumApp/Rnum.js'
+
+function App() {
+  return (
+    <div className="App">
+      <RandomNum />
+    </div>
+  );
+}
+
+export default App;
